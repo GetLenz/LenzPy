922dad6e67ce6ee02222d9df59dfc8e0b8e7430b
Raynor Elgie <elgieraynor@gmail.com>
2023-08-17 19:37:32 -0400

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='LenzPy',
+    version='0.1.0',
+    packages=['LenzPy'],
+    description='Python wrapper designed to facilitate interaction with the Lenz report API.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GetLenz/LenzPy',
+)
